101fda76ebd6d57a520006cc73c988b3a34da2c4
Thasleem C <37997373+thasleemmji@users.noreply.github.com>
2019-04-29 23:46:05 +0530

    Update login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,7 +2,7 @@
 <html>
 <head>
   <meta charset="utf-8" />
-  <title>Welcome | {{config('app.name')}}</title>
+  <title>Welcome | Agent Agreement System</title>
   <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
   <meta content="" name="description" />
   <!-- App favicon -->
